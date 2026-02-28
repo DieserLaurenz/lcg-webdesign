5ab45a80693d35021875e3f7b27f291c5b80fa7b
Laurenz Gilbert <@gilbert@uni-potsdam.de>
2026-02-28 14:20:30 +0100

    Restore original font rendering setup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", "./App.jsx"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-sans-local)"],
-        serif: ["var(--font-serif-local)"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
